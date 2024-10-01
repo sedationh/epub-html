5a13a6c6e9f3b9545f3a41642025892a64457644
SedationH <sedationh@gmail.com>
2024-10-01 16:55:51 +0800

    remove holes translate version

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
         <li>
           <li><a href="/outputs/Principles Life and Work/Principles Life and Work.html">Principles Life and Work</a></li>
 <li><a href="/outputs/Zero to One/Zero to One.html">Zero to One</a></li>
-<li><a href="/outputs/holes/holes.html">holes</a></li>
 <li><a href="/outputs/holes-en/holes-en.html">holes-en</a></li>
 
         </li>
