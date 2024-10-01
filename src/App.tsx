e602d6cb3f639e1dac701d3e41009aa801f31bd2
SedationH <sedationh@gmail.com>
2024-10-01 16:52:16 +0800

    add books

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
       <h1>Epub HTML</h1>
       <ul>
         <li>
-          <li><a href="/outputs/holes/holes.html">holes</a></li>
+          <li><a href="/outputs/Principles Life and Work/Principles Life and Work.html">Principles Life and Work</a></li>
+<li><a href="/outputs/Zero to One/Zero to One.html">Zero to One</a></li>
+<li><a href="/outputs/holes/holes.html">holes</a></li>
 <li><a href="/outputs/holes-en/holes-en.html">holes-en</a></li>
 
         </li>
