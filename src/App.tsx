260ff4562b2e0eee07806043801dd499c31f4517
SedationH <sedationh@gmail.com>
2024-10-06 23:15:07 +0800

    add books

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
       <ul>
         <li>
           <li><a href="/outputs/Principles Life and Work/Principles Life and Work.html">Principles Life and Work</a></li>
+<li><a href="/outputs/Steal Like an Artist 10 Things Nobody Told You About Being Creative (Austin Kleon) (Z-Library)/Steal Like an Artist 10 Things Nobody Told You About Being Creative (Austin Kleon) (Z-Library).html">Steal Like an Artist 10 Things Nobody Told You About Being Creative (Austin Kleon) (Z-Library)</a></li>
 <li><a href="/outputs/Zero to One/Zero to One.html">Zero to One</a></li>
 <li><a href="/outputs/holes-en/holes-en.html">holes-en</a></li>
 
