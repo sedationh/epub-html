9d96d685ee33945fd4077b449ed3981d57cf35c3
SedationH <sedationh@gmail.com>
2024-10-29 23:05:08 +0800

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-
 function App() {
   return (
     <>
       <h1>Epub HTML</h1>
       <ul>
         <li>
-          <li><a href="/outputs/Steal Like an Artist 10 Things Nobody Told You About Being Creative (Austin Kleon) (Z-Library)/Steal Like an Artist 10 Things Nobody Told You About Being Creative (Austin Kleon) (Z-Library).html">Steal Like an Artist 10 Things Nobody Told You About Being Creative (Austin Kleon) (Z-Library)</a></li>
-<li><a href="/outputs/The Almanack of Naval Ravikant (Eric Jorgenson) (Z-Library)/The Almanack of Naval Ravikant (Eric Jorgenson) (Z-Library).html">The Almanack of Naval Ravikant (Eric Jorgenson) (Z-Library)</a></li>
-<li><a href="/outputs/Zero to One/Zero to One.html">Zero to One</a></li>
-<li><a href="/outputs/holes/holes.html">holes</a></li>
-<li><a href="/outputs/原则-Principles Life and Work (Ray Dalio) (Z-Library)/原则-Principles Life and Work (Ray Dalio) (Z-Library).html">原则-Principles Life and Work (Ray Dalio) (Z-Library)</a></li>
-
+          <li>
+            <a href="/outputs/Principles Life and Work (Ray Dalio) (Z-Library)/Principles Life and Work (Ray Dalio) (Z-Library).html">
+              Principles Life and Work (Ray Dalio) (Z-Library)
+            </a>
+          </li>
+          <li>
+            <a href="/outputs/Steal Like an Artist 10 Things Nobody Told You About Being Creative (Austin Kleon) (Z-Library)/Steal Like an Artist 10 Things Nobody Told You About Being Creative (Austin Kleon) (Z-Library).html">
+              Steal Like an Artist 10 Things Nobody Told You About Being
+              Creative (Austin Kleon) (Z-Library)
+            </a>
+          </li>
+          <li>
+            <a href="/outputs/The Almanack of Naval Ravikant (Eric Jorgenson) (Z-Library)/The Almanack of Naval Ravikant (Eric Jorgenson) (Z-Library).html">
+              The Almanack of Naval Ravikant (Eric Jorgenson) (Z-Library)
+            </a>
+          </li>
+          <li>
+            <a href="/outputs/Zero to One/Zero to One.html">Zero to One</a>
+          </li>
+          <li>
+            <a href="/outputs/holes/holes.html">holes</a>
+          </li>
+          <li>
+            <a href="/outputs/原则-Principles Life and Work (Ray Dalio) (Z-Library)/原则-Principles Life and Work (Ray Dalio) (Z-Library).html">
+              原则-Principles Life and Work (Ray Dalio) (Z-Library)
+            </a>
+          </li>
         </li>
       </ul>
     </>
